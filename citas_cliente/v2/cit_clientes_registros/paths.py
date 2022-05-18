878ff1d47d0780d908627f55528700ccc00f0437
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-05-18 15:35:54 -0500

    Por probar registros.

--- citas_cliente/v2/cit_clientes_registros/paths.py
+++ citas_cliente/v2/cit_clientes_registros/paths.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 from lib.database import get_db
 
 from .crud import solicitar_nueva_cuenta, validar_nueva_cuenta, concluir_nueva_cuenta
-from .schemas import CitClienteRegistroIn, CitClienteRegistroOut, CitClienteRegistroValidarOut, CitClienteRegistroConcluirIn, CitClienteRegistroConcluirOut,
+from .schemas import CitClienteRegistroIn, CitClienteRegistroOut, CitClienteRegistroValidarOut, CitClienteRegistroConcluirIn, CitClienteRegistroConcluirOut
 
 cit_clientes_registros = APIRouter(prefix="/v2/nueva_cuenta", tags=["nueva cuenta"])
 
@@ -34,6 +34,13 @@ async def nueva_cuenta_validar(
     db: Session = Depends(get_db),
 ):
     """Quiero crear una nueva cuenta, viene del URL proporcionado, entrego el formulario para definir la contrasena"""
+    try:
+        cit_cliente_registro = validar_nueva_cuenta(db, hashid, cadena_validar)
+    except IndexError as error:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Not found: {str(error)}") from error
+    except ValueError as error:
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail=f"Not acceptable: {str(error)}") from error
+    return CitClienteRegistroValidarOut.from_orm(cit_cliente_registro)
 
 
 @cit_clientes_registros.post("/concluir", response_model=CitClienteRegistroConcluirOut)
@@ -42,3 +49,10 @@ async def nueva_cuenta_concluir(
     db: Session = Depends(get_db),
 ):
     """Quiero crear una nueva cuenta, recibo el formulario con la contrasena"""
+    try:
+        cit_cliente_registro = concluir_nueva_cuenta(db, registro)
+    except IndexError as error:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Not found: {str(error)}") from error
+    except ValueError as error:
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail=f"Not acceptable: {str(error)}") from error
+    return CitClienteRegistroConcluirOut.from_orm(cit_cliente_registro)
