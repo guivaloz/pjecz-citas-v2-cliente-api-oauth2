5933a63756464e92c6950b8d168216ed119ce2ca
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-06-01 15:22:56 -0500

    Ya se evitan los dias inhabiles, sabados y domingos

--- citas_cliente/v2/cit_horas_disponibles/crud.py
+++ citas_cliente/v2/cit_horas_disponibles/crud.py
@@ -5,8 +5,11 @@ from datetime import date, timedelta, datetime, time
 from typing import Any
 from sqlalchemy.orm import Session
 
+from ..cit_dias_inhabiles.crud import get_cit_dias_inhabiles
 from ..cit_servicios.crud import get_cit_servicio
 from ..oficinas.crud import get_oficina
+
+LIMITE_DIAS = 90
 
 
 def get_cit_horas_disponibles(
@@ -24,7 +27,23 @@ def get_cit_horas_disponibles(
     # Consultar el servicio
     cit_servicio = get_cit_servicio(db, cit_servicio_id)
 
-    # Validar fecha
+    # Validar que la fecha no sea del pasado
+    if fecha <= date.today():
+        raise IndexError("No puede agendar citas para hoy o en el pasado")
+
+    # Validar que la fecha no sea posterior al LIMITE_DIAS
+    if fecha > date.today() + timedelta(LIMITE_DIAS):
+        raise IndexError(f"No puede agendar citas en mas de {LIMITE_DIAS} dias")
+
+    # Validar que la fecha no sea sabado o domingo
+    if fecha.weekday() in (5, 6):
+        raise IndexError("No puede agendar citas en fines de semana")
+
+    # Validar que la fecha no sea un dia inhabil
+    cit_dias_inhabiles = get_cit_dias_inhabiles(db).all()
+    dias_inhabiles = [item.fecha for item in cit_dias_inhabiles]
+    if fecha in dias_inhabiles:
+        raise IndexError("No puede agendar citas en dias inhabiles")
 
     # Definir los tiempos de inicio, de termino y el timedelta de la duracion
     tiempo_inicial = datetime(
